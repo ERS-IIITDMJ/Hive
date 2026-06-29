4c00517078797b7d4b11b3e901c26fc79739f640
adarsh <adarshdeshmukh00000@gmail.com>
2026-06-29 23:14:16 +0530

    Init component page

---
+++ frontend/src/Components/ComponentsPage.jsx
@@ -0,0 +1,14 @@
+import {useState} from 'react';
+import { Search , Menu , X } from 'lucide-react';
+import SideBar from './sidebar';
+import SearchBar from './searchBar';
+
+function componentPage() {
+  return (
+    <>
+    
+    </>
+
+  )
+}
+export default componentPage;
